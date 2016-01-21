e374290796a0d71331bc587d41c3d9c0b6f5a97a
zengxianghong <zengxianghong@sailvan.com>
2016-01-21 09:36:43 +0800

    test

--- lib/smarty_sae/smarty.php
+++ lib/smarty_sae/smarty.php
@@ -3,11 +3,11 @@ include 'libs/Smarty.class.php';
 class AppSmarty extends Smarty{
 	public function __construct(){
 		parent::__construct();
-echo ENV;exit;
+
 		//$this->cache_lifetime = 30*24*3600; //更新周期
 		$this->template_dir		= WEB_PATH.'html/templates';
-		$this->compile_dir		= !defined('ENV') ? 'saemc://smartytpl/' : WEB_PATH.'lib/smarty_sae/template_c/';
-		$this->cache_dir		= !defined('ENV') ? 'saemc://smartytpl/' : WEB_PATH.'lib/smarty_sae/cache/';
+		$this->compile_dir		= !defined('ENV') && ENV != 'local' ? 'saemc://smartytpl/' : WEB_PATH.'lib/smarty_sae/template_c/';
+		$this->cache_dir		= !defined('ENV') && ENV != 'local' ? 'saemc://smartytpl/' : WEB_PATH.'lib/smarty_sae/cache/';
 		$this->compile_locking	= false; // 防止调用touch,saemc会自动更新时间，不需要touch
 		$this->caching			= false; //是否使用缓存，项目在调试期间，不建议启用缓存
 
