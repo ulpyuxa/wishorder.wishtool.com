5c6f475a4547b028401a7ad0260fca07a8ffa32b
zengxianghong <zengxianghong@sailvan.com>
2016-01-22 15:05:28 +0800

    调试smarty

--- action/wishListing.action.php
+++ action/wishListing.action.php
@@ -2,7 +2,7 @@
 class WishListingAct extends CommonAct{
 	public function act_wishListingList() {
 		$listingData = AliexListingModel::getListingData();
-		$this->smarty->assign('data', $listingData[0]);
+		//$this->smarty->assign('data', $listingData[0]);
 		$this->smarty->display('wishOrder.tpl');
 	}
 }
