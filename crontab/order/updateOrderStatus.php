d9e22d062905c19acc9694a1425cfbb4ea271e5c
zengxianghong <zengxianghong@sialvan.com>
2016-02-19 14:57:08 +0800

    修复bug

--- crontab/order/updateOrderStatus.php
+++ crontab/order/updateOrderStatus.php
@@ -8,8 +8,10 @@ error_reporting(E_ALL);
 define("SYSTEM_CRONTAB_USER","true");	//跳过所有权限验证
 set_time_limit(0);
 
+echo rtrim(str_replace(DIRECTORY_SEPARATOR, '/', __DIR__), 'crontab/order')."/framework.php";exit;
 include rtrim(str_replace(DIRECTORY_SEPARATOR, '/', __DIR__), 'crontab/order')."/framework.php";
 Core::getInstance();
 global $dbConn;
 
 $data	= WishProductModel::getWishProduct();
+var_dump($data);
