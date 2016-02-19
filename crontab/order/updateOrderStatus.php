4b464b166a4dd1e267e457d3564542883afcddec
zengxianghong <zengxianghong@sialvan.com>
2016-02-19 14:58:31 +0800

    ff

--- crontab/order/updateOrderStatus.php
+++ crontab/order/updateOrderStatus.php
@@ -8,6 +8,7 @@ error_reporting(E_ALL);
 define("SYSTEM_CRONTAB_USER","true");	//跳过所有权限验证
 set_time_limit(0);
 
+echo str_replace(DIRECTORY_SEPARATOR, '/', __DIR__),PHP_EOL;
 echo rtrim(str_replace(DIRECTORY_SEPARATOR, '/', __DIR__), 'crontab/order')."/framework.php";exit;
 include rtrim(str_replace(DIRECTORY_SEPARATOR, '/', __DIR__), 'crontab/order')."/framework.php";
 Core::getInstance();
