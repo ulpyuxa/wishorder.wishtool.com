aad6abd6f4fc687dc52a2d73d0171b6e321673e3
zengxianghong <zengxianghong@sialvan.com>
2016-02-20 15:29:40 +0800

    ss

--- action/common.action.php
+++ action/common.action.php
@@ -11,6 +11,7 @@ class CommonAct {
 	 * 功能: 检测是否登录
 	 */
 	public function _checkLogin() {
+		var_dump($_COOKIE);exit;
 		if(!isset($_COOKIE['USERINFO']) || isset($_COOKIE['USERINFO1'])) {
 			if(strtolower($_REQUEST['act']) != 'index' && strtolower($_REQUEST['mod']) != 'login') {
 				//echo $_REQUEST['act'], '    ', $_REQUEST['mod'];exit;
