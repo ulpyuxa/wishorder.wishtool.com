6d2011433b8f7e64c4381498290bd87ce2457e9d
zengxianghong <zengxianghong@sialvan.com>
2016-05-27 07:47:14 +0800

    修改SDK

--- lib/sdk/wishSdk/wishProduct.class.php
+++ lib/sdk/wishSdk/wishProduct.class.php
@@ -45,4 +45,53 @@ class WishProductApi extends WishBase {
 		$ret = $this->sendHttpRequest($para);
 		return $ret;
 	}
+	
+	/**
+	 * 功能: 创建商品主料号
+	 */
+	public function createProductSpu($data) {
+		$this->api = 'product';
+		$this->act = 'add';
+		$para	= array(
+			'main_image'		=> $data['main_image'],
+			'name'				=> $data['name'],
+			'description'		=> $data['description'],
+			'tags'				=> $data['tags'],
+			'sku'				=> $data['sku'],
+			'inventory'			=> $data['inventory'],
+			'price'				=> $data['price'],
+			'shipping'			=> $data['shipping'],
+			'extra_images'		=> $data['extra_images'],
+			'parent_sku'		=> $data['parent_sku'],
+			'msrp'				=> $data['msrp'],
+			'color'				=> $data['color'],
+			'size'				=> $data['size'],
+			'brand'				=> $data['brand'],
+			'shipping_time'		=> $data['shipping_time'],
+			'landing_page_url'	=> $data['landing_page_url'],
+			'upc'				=> $data['upc'],
+		);
+		return $ret = $this->sendHttpRequest($para);
+	}
+
+	/**
+	 * 功能：创建商品子料号
+	 */
+	public function createProductSku($data) {
+		$this->api = 'variant';
+		$this->act = 'add';
+		$para	= array(
+			'sku'			=> $data['sku'],
+			'inventory'		=> $data['inventory'],
+			'price'			=> $data['price'],
+			'size'			=> $data['size'],
+			'shipping'		=> $data['shipping'],
+			'parent_sku'	=> $data['parent_sku'],
+			'color'			=> $data['color'],
+			'msrp'			=> $data['msrp'],
+			'main_image'	=> $data['main_image'],
+			'shipping_time'	=> $data['shipping_time'],
+		);
+		return $ret = $this->sendHttpRequest($para);
+	}
 }
