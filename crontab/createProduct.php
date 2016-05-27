e4fbdddc174f672531e839c6d8f9362a45785497
zengxianghong <zengxianghong@sialvan.com>
2016-05-27 08:00:21 +0800

    增加进度数量判断

--- crontab/createProduct.php
+++ crontab/createProduct.php
@@ -4,6 +4,10 @@
 author: zxh
  * 日期: 2016/1/23 23:04
  */
+exec('ps aux|grep '.WEB_PATH.'crontab/'.basename(__FILE__).'|grep -v grep',$ps);
+if(count($ps)>1){
+ 	exit();
+}
 error_reporting(E_ALL);
 define("SYSTEM_CRONTAB_USER", "true"); //跳过所有权限验证
 set_time_limit(0);
