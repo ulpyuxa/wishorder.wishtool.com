edcd3296db8839a9528f7677dc054d1d0ccc3ec6
zengxianghong <zengxianghong@sailvan.com>
2016-09-05 15:34:47 +0800

    将账号简称更改为大写

--- action/wishProduct.action.php
+++ action/wishProduct.action.php
@@ -147,7 +147,7 @@ class WishProductAct extends CommonAct{
 		$hasTags	= false;
 		$ret		= array();
 		$account	= isset($_COOKIE['account']) ? $_COOKIE['account'] : 'geshan0728';
-		$accountAbbr= C('ACCOUNTABBR')[$account];
+		$accountAbbr= strtoupper(C('ACCOUNTABBR')[$account]);
 		$price		= WishProductModel::spuPrice($spu);		//获取价格已经减去$1的运费
 		foreach($dataArr as $k => $v) {
 			if(strlen($v) < 100) {
