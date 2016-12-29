f23b30a3e2ee591a11c08a648dd4b7b6027a8a26
曾祥红 <zengxianghong@sailvan.com>
2016-12-29 10:34:59 +0800

    增加获取外网IP的接口

--- crontab/pi_dospy.php
+++ crontab/pi_dospy.php
@@ -1,4 +1,7 @@
 <?php
+//$str = "{ip:'183.238.188.194',address:'广东省深圳市 移动'}";
+//preg_match('/\b\d+\.\d+\.\d+\.\d+/', $str, $arr);
+//PRINT_R($arr);EXIT;
 function post_data($url, $data) {
 	if ($url == '' || !is_array($data)) {
 		return false;
@@ -7,7 +10,6 @@ function post_data($url, $data) {
 	if (!$ch) {
 		exit(json_encode(array('msg' => '内部错误：服务器不支持CURL')));
 	} 
-
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_POST, 1);
 	curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -20,12 +22,11 @@ function post_data($url, $data) {
 	curl_close($ch);
 	return $result;
 } 
-//获取域名的记录信息
 function domainList($domainId) {
 	$url = 'https://dnsapi.cn/Record.List';
 	$config = array(
 		'login_email' => 'ulpyuxa@163.com',
-		'login_password' => 'pdcxaje127',
+		'login_password' => 'pdcxaje127,',
 		'format' => 'json',
 		'lang' => 'cn',
 		'error_on_empty' => 'no',
@@ -34,12 +35,11 @@ function domainList($domainId) {
 	$post_data = post_data($url, $config);
 	return json_decode($post_data, true);
 } 
-//解析域名
 function updateDomain($para) {
 	$url = 'https://dnsapi.cn/Record.Modify';
 	$config = array(
 		'login_email' => 'ulpyuxa@163.com',
-		'login_password' => 'pdcxaje127',
+		'login_password' => 'pdcxaje127,',
 		'format'			=> 'json',
 		'lang'				=> 'cn',
 		'error_on_empty'	=> 'no',
@@ -53,12 +53,11 @@ function updateDomain($para) {
 	$post_data = post_data($url, $config);
 	return json_decode($post_data, true);
 }
-//获取域名信息
 function getDomainInfo() {
 	$url = 'https://dnsapi.cn/Domain.List';
 	$config = array(
 		'login_email' => 'ulpyuxa@163.com',
-		'login_password' => 'pdcxaje127',
+		'login_password' => 'pdcxaje127,',
 		//'token' => '12763,70bea707121e3c7f247f39eee265e63a',
 		'format' => 'json',
 		'lang' => 'cn',
@@ -67,25 +66,73 @@ function getDomainInfo() {
 	$post_data = post_data($url, $config);
 	return json_decode($post_data, true);
 }
-function getIp() {
-	$myip	= file_get_contents('http://ip.taobao.com/service/getIpInfo2.php?ip=myip');
-	$ipInfo	= json_decode($myip, true);
-	$ip		= $ipInfo['data']['ip'];
-	return $ip;
+function getIp1() {		//用来获取路由器的外网IP
+	//$url = 'http://ip.taobao.com/service/getIpInfo2.php?ip=myip';
+	$url = 'http://ip.chinaz.com/getip.aspx';
+	ini_set('user_agent', "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; GreenBrowser)");
+	$opts = array( 
+			'http' => array (
+				'method'	=> "GET",
+				'timeout'	=> 3,
+			)
+		);
+	$myIp		= @file_get_contents($url, false, stream_context_create($opts));
+	$str = $myIp;
+	preg_match('/\b\d+\.\d+\.\d+\.\d+/', $str, $arr);
+	return $arr[0];
 }
+function getIp2() {		//用来获取路由器的外网IP
+	$url = 'http://members.3322.org/dyndns/getip';
+	ini_set('user_agent', "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; GreenBrowser)");
+	$opts = array( 
+			'http' => array (
+				'method'	=> "GET",
+				'timeout'	=> 3,
+			)
+		);
+	$myIp		= @file_get_contents($url, false, stream_context_create($opts));
+	return trim($myIp);
+}
+function getIp3() {
+    $url = 'http://www.ip138.com/ip2city.asp';
+	ini_set('user_agent', "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 2.0.50727; .NET CLR 3.0.04506.30; GreenBrowser)");
+	$opts = array( 
+			'http' => array (
+				'method'	=> "GET",
+				'timeout'	=> 3,
+			)
+		);
+	$html		= @file_get_contents($url, false, stream_context_create($opts));
+    preg_match('/\[(.*)\]/', $html, $myIp);
+    return $myIp[1];
+ }
 $domainInfo = getDomainInfo();
-$domianId	= '';
+$domainId	= 0;
 foreach($domainInfo['domains'] as $key => $val) {
-	if($val['name'] === 'wishtool.cn') {
-		$domianId = $val['id'];
+	if($val['name'] === 'wiitool.com') {
+		$domainId = $val['id'];
 	}
 }
-$records = domainList($domainInfo['domains'][0]['id']);		//暂时只更改域名：wishtool.cn
-$myIp	= getIp();
+if(empty($domainId)) {
+	exit('本次未获取到domainId');
+}
+$records = domainList($domainId);
+$myIp	= getIp1();
+if(empty($myIp)) {
+	$myIp	= getIp2();
+}
+if(empty($myIp)) {
+	$myIp	= getIp3();
+}
+echo $myIp, PHP_EOL;
+if(empty($myIp)) {
+	exit('本次未获取到IP，等待下次重试');
+}
+$siteArr= array('pi-order','order', 'mysql', 'www','invoice');
 foreach($records['records'] as $k => $v) {
-	if(in_array($v['name'],array('pi-phpmyadmin','pi-order', 'www')) && $v['value'] !== $myIp) {
+	if(in_array($v['name'],$siteArr) && $v['value'] !== $myIp) {
 		$para = array(
-			'domain_id'		=> $domianId,
+			'domain_id'		=> $domainInfo['domains'][0]['id'],
 			'record_id'		=> $v['id'],
 			'sub_domain'	=> $v['name'],
 			'value'			=> $myIp,
@@ -96,7 +143,7 @@ foreach($records['records'] as $k => $v) {
 		if(intval($status['status']['code']) === 1) {
 			echo $v['name']. '修改成功! 原因:ip不一致原始绑定IP:'.$v['value'].', 当前服务器IP:'.$myIp, PHP_EOL;
 		}
-	} else if(in_array($v['name'],array('pi-phpmyadmin','pi-order', 'www')) && $v['value'] === $myIp) {
+	} else if(in_array($v['name'], $siteArr) && $v['value'] === $myIp) {
 		echo '子域名：'.$v['name'].'不需要进行修改！', PHP_EOL;
 	}
 }
