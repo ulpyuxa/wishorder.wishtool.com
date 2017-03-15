fe18fe128690a64154f3dae723b21279c1fad078
ulpyuxa@163.com <ulpyuxa@163.com>
2017-03-15 14:09:48 +0800

    test

--- action/wishOrder.action.php
+++ action/wishOrder.action.php
@@ -13,9 +13,17 @@ class WishOrderAct extends CommonAct{
 	 */
 	public function act_wishOrderSync($start = 0, $count = 50) {
 		$account		= $_REQUEST['account'];
+		echo $account, ' --- ', $start, '----' , $count, PHP_EOL;
 		$wishOrderApi	= new WishOrderApi($account, 1);
 		$ret			= $wishOrderApi->getAllorder($start, $count);
 		$insert			= WishOrderModel::addOrder($ret, $account);
+		if(isset($ret['paging']['next'])) {
+			$urlInfo = parse_url($ret['paging']['next']);
+			$urlInfo = explode('&', $urlInfo['query']);
+			$start = str_replace('count=',"", $urlInfo[0]);
+			$end = str_replace('start=',"", $urlInfo[1]);
+			self::act_wishOrderSync($end);
+		}
 		if(!$insert) {
 			self::$errCode	= WishOrderModel::$errCode;
 			self::$errMsg	= WishOrderModel::$errMsg;
